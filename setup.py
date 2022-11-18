b7143e7b57d89a531923c1afd115e06360205360
Gonçalo Leal <gleal@ubiwhere.com>
2022-11-18 18:07:51 +0000

    version 0.2.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-VERSION = "0.1.0"
+VERSION = "0.2.2"
 
 install_requires = [
     "acme>=0.29.0",
@@ -16,17 +16,16 @@ setup(
     name="certbot-dns-dreamhost",
     version=VERSION,
     description="DreamHost DNS Authenticator plugin for Certbot",
-    url="",
+    url="https://github.com/goncalo-leal/certbot-dns-dreamhost",
     author="Gonçalo Leal",
     author_email="goncalolealsilva@ua.pt",
     license="Apache License 2.0",
-    package=find_packages(),
+    package="src/certbot_dns_dreamhost",
     include_package_data=True,
     install_requires=install_requires,
     entry_points={
         "certbot.plugins": [
-            "dns-dreamhost-authenticator = certbot_dns_dreamhost.dns_dreamhost:Authenticator"
+            "dns-dreamhost = certbot_dns_dreamhost.dns_dreamhost:Authenticator"
         ]
     },
-    test_suite="certbot_dns_dreamhost",
 )
